c9ea1f96dbadca2cb9f6bc08a1a226f4ee2413ca
TomyRojasPonce <111310431+TomyRojasPonce@users.noreply.github.com>
2025-10-28 08:33:32 -0300

    fix
    uhiu

---
+++ src/main/java/repository/cassandra/CassandraRepository.java
@@ -0,0 +1,13 @@
+package repository.cassandra;
+
+public class CassandraRepository {
+    private static CassandraRepository instance;
+
+    private CassandraRepository() {}
+
+    public static CassandraRepository getInstance(){
+        if (instance == null)
+            instance = new CassandraRepository();
+        return instance;
+    }
+}
